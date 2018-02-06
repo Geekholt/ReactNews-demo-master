d660e0d0703d6c048c049b99617d968ec9c7d389
geekholt <654207931@qq.com>
2018-02-06 14:55:30 +0800

    PC端页头组件开发

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from 'antd/lib/button';
 import 'antd/dist/antd.css';
+import PCHeader from "./component/pc_header";
+import "../css/pc.css";
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <div className="App">
-                    <Button type="primary">Button</Button>
-                </div>
+               <PCHeader/>
             </div>
         );
     };
---
+++ src/js/component/pc_index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PCHeader from "./pc_header";
+
+export default class PCIndex extends React.Component {
+    render() {
+        return (
+            <PCHeader/>
+        );
+    };
+};
